fcd4bac72e2db1b3fccb7641faea4a0a2fe8cb38
hochgi <gilad.hoch@sparkbeyond.com>
2021-01-31 21:30:07 +0200

    scala-parser-combinators implementation

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,4 +3,5 @@ import sbt._
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.3"
   lazy val scalacheck = "org.scalacheck" %% "scalacheck" % "1.15.2"
+  lazy val scalaParserCombinators = "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
 }
--- src/main/scala/sb/be/guildexercises/parsing/Tree.scala
+++ src/main/scala/sb/be/guildexercises/parsing/Tree.scala
@@ -1,7 +1,10 @@
 package sb.be.guildexercises.parsing
 
+import scala.util.parsing.combinator._
+
 final case class Tree(value: String, children: List[Tree])
 object Tree {
+
   def format(tree: Tree): String = {
     val sb = new StringBuilder
     def inner(subTree: Tree): Unit = {
@@ -17,5 +20,17 @@ object Tree {
     inner(tree)
     sb.result()
   }
-  def parse(input: String): Option[Tree] = ???
+
+  object TreeParser extends RegexParsers {
+    def value:    Parser[String]     = "[A-Za-z0-9]*".r
+    def children: Parser[List[Tree]] = "(" ~> rep1sep(tree, ",") <~ ")"
+    def tree:     Parser[Tree]       = (value ~ children) ^^ { case v ~ ts => Tree(v,ts) } | value ^^ (Tree(_,Nil))
+
+    def parse(input: String): Option[Tree] = parseAll[Tree](tree, input) match {
+      case Success(tree, _) => Some(tree)
+      case _                => None
+    }
+  }
+
+  def parse(input: String): Option[Tree] = TreeParser.parse(input)
 }
