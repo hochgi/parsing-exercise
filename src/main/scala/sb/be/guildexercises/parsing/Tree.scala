6672dd64428f7ea303e19dac7d54c65263c291d5
hochgi <gilad.hoch@sparkbeyond.com>
2021-01-31 20:50:38 +0200

    format simple implementation

--- src/main/scala/sb/be/guildexercises/parsing/Tree.scala
+++ src/main/scala/sb/be/guildexercises/parsing/Tree.scala
@@ -2,6 +2,20 @@ package sb.be.guildexercises.parsing
 
 final case class Tree(value: String, children: List[Tree])
 object Tree {
-  def format(tree: Tree): String = ???
+  def format(tree: Tree): String = {
+    val sb = new StringBuilder
+    def inner(subTree: Tree): Unit = {
+      sb ++= subTree.value
+      var ch = '('
+      subTree.children.foreach { childNode =>
+        sb += ch
+        ch = ','
+        inner(childNode)
+      }
+      if(ch == ',') sb += ')'
+    }
+    inner(tree)
+    sb.result()
+  }
   def parse(input: String): Option[Tree] = ???
 }
